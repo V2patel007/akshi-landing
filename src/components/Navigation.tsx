913ba2872f3c811d1a64ffa5cfda4ea71754c378
Viraj Patel <130348375+V2patel007@users.noreply.github.com>
2025-08-11 23:19:23 +0530

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navigation: React.FC = () => {
   return (
-    <header className="fixed top-0 left-0 right-0 z-40 bg-transparent">
+    <header className="absolute top-0 left-0 right-0 z-40 bg-transparent">
       <nav className="container mx-auto px-6 py-6">
         <div className="flex justify-center items-center">
           <img 
