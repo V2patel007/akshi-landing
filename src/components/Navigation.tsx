a8d459b318c972c5400b73eb5b3b857b130947c9
Viraj Patel <130348375+V2patel007@users.noreply.github.com>
2025-08-11 22:54:07 +0530

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation: React.FC = () => {
   return (
     <header 
-      className="top-0 left-0 right-0 z-40">
+      className="fixed top-0 left-0 right-0 z-40">
       <nav className="container mx-auto px-6 py-6">
         <div className="flex justify-between items-center">
           <div className="flex-1"></div>
